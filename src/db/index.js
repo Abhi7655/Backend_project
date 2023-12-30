d773cdf4c94eb31518479c7934e011cd160109d6
abc <abc@abcs-MacBook-Air.local>
2023-12-31 01:36:25 +0530

    urilsMiddlewareSetup

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ try {
      const connectionDatabase = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
        console.log(`Connection with Mongoose is estabished: ${connectionDatabase.connection.host} `)
         }catch (error) {
-         console.log(error)   
+         console.log(`Error is found at error`)   
     }
 }
 export default connectDb;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
 import dotenv from 'dotenv';
 import connectDb from './db/index.js';
+import { app } from './app.js';
 dotenv.config({
     path:'./env'
 })
-connectDb();
+connectDb().
+then(
+    app.listen(process.env.PORT|| 8000,()=>{
+         console.log(`app listening at PORT ${process.env.PORT}`)
 
-
- 
-// ( async()=>{
-// try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
-//     app.on("error",(error)=>{
-//         console.log("ERR:",error)
-//         throw err
-//     })
-//     app.listen(process.env.PORT,()=>{
-//         console.log(`app is listening on ${process.env.PORT}`)
-//     })
-// } catch (error) {
-//     console.log(error)
-//     throw err
-// }
-// })()
+    })
+).
+catch((error)=>{
+        console.log("MONGO db connection Failed ",error)
+    }
+);
